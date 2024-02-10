70ede5c0aa45bc270956ab787c1a24314d89d2d7
Gilang <ondozwahyudi@gmail.com>
2024-02-11 01:21:54 +0700

    [update authrezation]

--- app/(platform)/protected/page.tsx
+++ app/(platform)/protected/page.tsx
@@ -1,9 +1,13 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth, useUser } from "@clerk/nextjs";
 
 const ProtectedPage = () => {
+  const { sessionId } = useAuth();
+
+  const { user } = useUser();
   return (
     <div>
+      <h1>Protected Page {sessionId}</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
   );
